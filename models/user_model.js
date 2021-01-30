d8c786eb1b2d71551afa3a603cc16486d31a1a3c
Mustafaryad <mustafariad03@gmail.com>
2021-01-30 07:08:40 +0200

    change supervisior role to agancy

--- models/user_model.js
+++ models/user_model.js
@@ -26,7 +26,7 @@ const UserSchema = new mongoose.Schema({
   role: {
     type: String,
     default:"basic",
-    enum:["basic","supervisor","admin"]
+    enum:["basic","agancy","admin"]
   },
   isactive:{
     type:Boolean,
