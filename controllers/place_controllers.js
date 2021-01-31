5b8b6ef686286d187dcdf5c1f5d1275e6f45e609
Mustafaryad <mustafariad03@gmail.com>
2021-01-31 15:27:58 +0200

    make relation with place,comment and user

--- controllers/place_controllers.js
+++ controllers/place_controllers.js
@@ -1,6 +1,7 @@
 const Place = require("../models/place_model");
+const Comment = require("../models/comment_model");
 
-addPlace = (req, res) => {
+const addPlace = (req, res) => {
   const body = req.body;
   if (!body) {
     return res.status(400).json({
@@ -12,9 +13,12 @@ addPlace = (req, res) => {
   const place = new Place(body);
 
   if (!place) {
-    return res.status(400).json({ success: false, error: err });
+    return res.status(400).json({
+      success: false,
+      error: err
+    });
   }
-
+  place.owner = req.user._id;
   place
     .save()
     .then(() => {
@@ -30,50 +34,108 @@ addPlace = (req, res) => {
         message: "Place item not created",
       });
     });
-//   Place.create({
-//         title: req.body.title,
-//         description: req.body.description,
-//         location: req.body.location,
-//         type: req.body.type,
-//         tags: req.body.tags,
-//         workStart: req.body.workStart,
-//         workEnd: req.body.workEnd,
-//         vistorType: req.body.vistorType,
-//         budgetType: req.body.budgetType,
-//         phone: req.body.phone,
-//         placeImages: req.body.placeImages,
-//     },
-//     (err, place) => {
-//         if (err) res.status(500).send(err)
-//         res.status(200).send(place)
-//     })
+
 };
 
-getAllPlaces = async (req, res) => {
-  await Place.find({}, (err, places) => {
+const getAllPlaces = async (req, res) => {
+  await Place.find({}).populate({
+    path: 'owner',
+    select: 'username'
+  }).populate({
+    path:'comments',
+    select:'text',
+    populate:{
+      path:"user",
+      select:"username"
+    }
+  }).exec((err, places) => {
     if (err) {
-      return res.status(400).json({ success: false, error: err });
+      return res.status(400).json({
+        success: false,
+        error: err
+      });
     }
     if (!places.length) {
-      return res.status(404).json({ success: false, error: `Place not found` });
+      return res.status(404).json({
+        success: false,
+        error: `Place not found`
+      });
     }
-    return res.status(200).json({ success: true, data: places });
+    return res.status(200).json({
+      success: true,
+      data: places
+    });
   }).catch((err) => console.log(err));
 };
 
-updatePlace = async (req, res) => {
-  let { ...data } = req.body; 
-  const result = await Place.findById({ _id: req.params.id }, data, {
+const updatePlace = async (req, res) => {
+  let {
+    ...data
+  } = req.body;
+  const result = await Place.findById({
+    _id: req.params.id
+  }, data, {
     new: true,
   });
   res.send(result);
 };
 
-deletePlace = async (req, res) => {
-  Place.findByIdAndDelete({ _id: req.params.id })
+const deletePlace = async (req, res) => {
+  Place.findByIdAndDelete({
+      _id: req.params.id
+    })
     .then((data) => res.json(data))
     .catch(next);
-    res.send("Delete " + result);
-  }
+  res.send("Delete " + result);
+}
 
-module.exports = { addPlace, getAllPlaces, updatePlace, deletePlace };
+const addCommentToPlace = async (req, res) => {
+
+  const place_id = req.params.id;
+  const place = await Place.findById(place_id)
+  const body = req.body;
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must add comment",
+    });
+  }
+  const comment = new Comment(body);
+  if (!comment) {
+    return res.status(400).json({
+      success: false,
+      error: err
+    });
+  }
+  comment.user = req.user._id;
+  comment
+    .save()
+    .then((comment) => {
+      place.comments.push(comment)
+      place.save();
+      console.log(comment.populate('user'));
+      return res.status(200).json({
+        success: true,
+        id: comment._id,
+        message: "Comment item created",
+      });
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        error,
+        message: "Comment item not created",
+      });
+    });
+
+
+}
+
+
+
+module.exports = {
+  addPlace,
+  getAllPlaces,
+  updatePlace,
+  deletePlace,
+  addCommentToPlace
+};
--- routes/placeRoutes.js
+++ routes/placeRoutes.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 const placeControllers = require("../controllers/place_controllers");
-
+const {requireAuth,grantAccess} =  require('../middleware/authMiddleware')
 
 router.get("/", placeControllers.getAllPlaces);
-router.post("/create", placeControllers.addPlace);
+router.post("/create", requireAuth,placeControllers.addPlace);
 router.put("/:id", placeControllers.updatePlace);
 router.delete("/place/:id", (req, res, next) => {
   Place.findOneAndDelete({ _id: req.params.id })
     .then((data) => res.json(data))
     .catch(next);
 });
+
+// add comment to place
+router.post("/create-comment/:id",requireAuth,placeControllers.addCommentToPlace);
 
 module.exports = router;
