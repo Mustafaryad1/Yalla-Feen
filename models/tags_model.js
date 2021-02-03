e12097073bb63597078fd55d5d16ef93e4747d25
Souad-Omar <souadar030@gmail.com>
2021-02-03 21:13:42 +0300

    Add tags controller, model and routes

---
+++ models/tags_model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Tags = mongoose.model(
+  "Tags",
+  new mongoose.Schema({
+    title: {
+      type:String,
+      required:true
+    },
+    places: [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Place"
+    }]
+  },{
+    timestamps:true
+  })
+);
+
+module.exports = Tags;
---
+++ routes/tags_routes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const tagsControllers = require("../controllers/tags_controllers");
+
+router.get("/", tagsControllers.getAllTags);
+router.post("/create", tagsControllers.addTags);
+router.delete("/tags/:id", (req, res, next) => {
+  Place.findOneAndDelete({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+module.exports = router;
